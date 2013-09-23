9a22f6be83bd0c264a7733b55baa6c271689e59a
Anthony Scatchell <ascatche@thoughtworks.com>
2013-09-23 05:37:33 +0300

    Refactored out randobot object, also fixed user quitting

--- randobot.rb
+++ randobot.rb
@@ -1,14 +1,35 @@
 puts "Welcome to randobot 6000"
 #@people = (1...7).to_a
-people = [:Remo, :Neil, :Godwin]
+#
+class Randobot
+    def initialize
+        @default_list = [:Remo, :Neil, :Godwin]
+        @people = original_list
+    end
+
+    def original_list
+        @default_list.clone
+    end
+
+    def next_person
+        person = @people.sample
+        @people.delete(person)
+        p @people
+        if (@people.size == 0)
+            @people = original_list
+        end
+
+        person
+    end
+end
 
 user_input = ""
 
-while (user_input != 'q')
-    person = people.sample
+randobot = Randobot.new
+
+while (!user_input.include? 'q')
+    person = randobot.next_person
     puts person
-    people.delete(person)
-    p people
     `say "It is #{person}'s turn."`
     user_input = gets
 end
