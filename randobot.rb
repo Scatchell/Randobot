0762b4f1165c1fba349c54c9421bc9ad34bed6ec
Anthony Scatchell <ascatche@thoughtworks.com>
2013-09-23 05:26:21 +0300

    Made people a local variable and removed randomize function

--- randobot.rb
+++ randobot.rb
@@ -1,18 +1,14 @@
 puts "Welcome to randobot 6000"
-@people = (1...7).to_a
-@people = [:Remo, :Neil, :Godwin]
-
-def randomize people
-    people.sample
-end
+#@people = (1...7).to_a
+people = [:Remo, :Neil, :Godwin]
 
 user_input = ""
 
 while (user_input != 'q')
-    person = randomize(@people)
+    person = people.sample
     puts person
-    @people.delete(person)
-    p @people
+    people.delete(person)
+    p people
     `say "It is #{person}'s turn."`
     user_input = gets
 end
