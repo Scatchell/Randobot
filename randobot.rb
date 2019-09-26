cefa52b4744d778dda2dd6f47216aeef3eb7cae3
Anthony Scatchell <scatchell@gmail.com>
2019-09-26 16:11:24 +0100

    Reformatting

--- randobot.rb
+++ randobot.rb
@@ -1,30 +1,30 @@
-#!/Users/ThoughtWorks/.rvm/rubies/ruby-2.0.0-p451/bin/ruby
+#!/Users/ascatche/.rvm/rubies/ruby-2.1.2/bin/ruby
 puts "Welcome to randobot 7000"
 
 class Randobot
-    def initialize
-        @default_list = [:Leeza, :Musa, :Kuana, :Jonah, :Raymond, :Tsepo, :Deon, :Tsatsi]
-        @people = original_list
+  def initialize
+    @default_list = [:kavita, :jenny, :arpitha, :rebeca, :claud, :bonnie]
+    @people = original_list
+  end
+
+  def original_list
+    @default_list.clone
+  end
+
+  def next_person
+    person = @people.sample
+    @people.delete(person)
+    p @people
+    if (@people.size == 0)
+      @people = original_list
     end
 
-    def original_list
-        @default_list.clone
-    end
-
-    def next_person
-        person = @people.sample
-        @people.delete(person)
-        p @people
-        if (@people.size == 0)
-            @people = original_list
-        end
-
-        person
-    end
+    person
+  end
 end
 
 def pressed_quit user_input
-    user_input.include? 'q'
+  user_input.include? 'q'
 end
 
 randobot = Randobot.new
@@ -32,8 +32,8 @@ randobot = Randobot.new
 user_input = ''
 
 while (!pressed_quit user_input)
-    person = randobot.next_person
-    puts person
-    `say "It is #{person}'s turn."`
-    user_input = gets
+  person = randobot.next_person
+  puts person
+  `say "#{person}"`
+  user_input = gets
 end
