8d06d5ed36785992656bf83c692bf9d4d9c149c6
Anthony Scatchell <scatchell@gmail.com>
2019-09-26 20:57:19 +0100

    Loads previous position of selected names each time

--- randobot.rb
+++ randobot.rb
@@ -2,34 +2,45 @@
 puts "Welcome to randobot 7000"
 
 class Randobot
+  attr_reader :people
+
   def initialize
     @default_list = [:astrid, :blair, :bonnie, :george, :humberto, :ian,  :paul, :reece, :richard, :ross, :rushil, :anthony]
-    @people = original_list
-  end
-
-  def original_list
-    @default_list.clone
+    p load_last_known_people
+    if !load_last_known_people.empty?
+      @people = load_last_known_people.clone
+    else
+      @people = @default_list.clone
+    end
   end
 
   def next_person
     person = @people.sample
     @people.delete(person)
-    p @people
+
     if (@people.size == 0)
-      @people = original_list
+      @people = @default_list.clone
     end
 
     person
   end
 
   def save
-    File.open("./persons.txt", "w") do |file|
+    File.open("./people.txt", "w") do |file|
       @people.each do |person|
         file.puts person
       end
     end
   end
 
+  def load_last_known_people
+    people = []
+    File.open("./people.txt", "r").each do |line|
+      people.push line.strip
+    end
+
+    people
+  end
 end
 
 def pressed_quit user_input
@@ -40,12 +51,17 @@ randobot = Randobot.new
 
 user_input = ''
 
+randobot.load_last_known_people
+
 while (!pressed_quit user_input)
   person = randobot.next_person
   secondPerson = randobot.next_person
   personStatement = person.to_s + ' and ' + secondPerson.to_s
+
   puts personStatement
+  puts "remaining: " + randobot.people.to_s
   `say "#{personStatement}"`
+
   user_input = gets
 end
 
