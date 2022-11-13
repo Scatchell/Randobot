65c7b67a594f4c1c812eea1354693cd4f2f6d2f8
Anthony Scatchell <ascatche@thoughtworks.com>
2022-11-13 15:31:56 +0100

    NOJIRA | Emma, Anthony | Pair selector will produce round-robin of all matches

--- pair_selector.rb
+++ pair_selector.rb
@@ -1,18 +1,50 @@
 class PairSelector
   def initialize(list_of_names)
     @list_of_names = list_of_names
+    @bye = "You'll forever be alone"
   end
+
   def all_possible_pairs
-    num_of_groups = @list_of_names.length / 2
+    players = evenise_players
+    all_rounds = []
+    number_of_players = players.length
+    num_of_rounds = number_of_players - 1
 
-    names_in_two_lists = @list_of_names.each_slice(num_of_groups).to_a
+    (1..num_of_rounds).each {
+      players = rotate_players(players)
+      all_rounds.push find_round_matches(players)
+    }
 
-    all_combos_between_two_lists = names_in_two_lists[0].product(names_in_two_lists[1]).to_a
-    all_combos_in_first_list = names_in_two_lists[0].combination(2).to_a
-    all_combos_in_second_list = names_in_two_lists[1].combination(2).to_a
-
-    all_combos = all_combos_between_two_lists.concat(all_combos_in_first_list).concat(all_combos_in_second_list).to_a
-
-    all_combos.each_slice(num_of_groups).to_a
+    all_rounds
   end
+
+  private
+
+  def evenise_players
+    players = @list_of_names.clone
+    if players.length % 2 != 0
+      players.push(@bye)
+    end
+    players
+  end
+
+  def find_round_matches(players)
+    player_sets = players.each_slice(players.length / 2).to_a
+    player_sets[0].map.with_index { |element, index| [element, player_sets[1][index]] }
+  end
+
+  def rotate_players(players)
+    players_in_two_lists = players.each_slice(players.length / 2).to_a
+
+    first_player_list = players_in_two_lists[0]
+    second_player_list = players_in_two_lists[1]
+    static_player = first_player_list.shift
+    second_player_list.push first_player_list.pop
+    first_player_list.unshift second_player_list.shift
+
+    first_player_list.unshift static_player
+
+    first_player_list.concat second_player_list
+  end
+
 end
--- spec/pair_selector_spec.rb
+++ spec/pair_selector_spec.rb
@@ -13,16 +13,37 @@ describe PairSelector do
   context "Should create groupings of pairs" do
 
     it "should generate a list of all possible pairs from four items" do
-      ps = PairSelector.new ['a', 'b', 'c', 'd']
+      ps = PairSelector.new [1, 2, 3, 4]
       all_possible_pairs = ps.all_possible_pairs
-
-      print_all_possible_pairs(all_possible_pairs)
-
       expect(all_possible_pairs).to eq [
-                                         [["a", "c"], ["a", "d"]],
-                                         [["b", "c"], ["b", "d"]],
-                                         [["a", "b"], ["c", "d"]]
+                                         [[1, 4], [3, 2]],
+                                         [[1, 2], [4, 3]],
+                                         [[1, 3], [2, 4]],
                                        ]
     end
+
+    it "should generate a list of all possible pairs from six items" do
+      ps = PairSelector.new [1, 2, 3, 4, 5, 6]
+      all_possible_pairs = ps.all_possible_pairs
+
+      expect(all_possible_pairs).to eq [[[1, 5], [4, 6], [2, 3]],
+                                        [[1, 6], [5, 3], [4, 2]],
+                                        [[1, 3], [6, 2], [5, 4]],
+                                        [[1, 2], [3, 4], [6, 5]],
+                                        [[1, 4], [2, 5], [3, 6]],
+                                       ]
+    end
+
+    it "should add a break when the number of players is uneven" do
+      ps = PairSelector.new [1, 2, 3]
+      all_possible_pairs = ps.all_possible_pairs
+
+      expect(all_possible_pairs).to eq [
+                                         [[1, "You'll forever be alone"], [3, 2]],
+                                         [[1, 2], ["You'll forever be alone", 3]],
+                                         [[1, 3], [2, "You'll forever be alone"]],
+                                       ]
+    end
+
   end
 end
